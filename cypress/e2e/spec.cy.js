8fb7b8cd05113e8c341ffb83ff6ac619deb010ea
Moch Fajrul Falah <mfajrulfalah.ff@gmail.com>
2022-11-30 10:58:27 +0700

    rapi + fixed#3

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -26,34 +26,33 @@ describe("Ajukan Mentor", () => {
     cy.get('#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(2) > .form-control').type(last_degree);
     cy.get("#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(3) > .form-control").type(occupation);
     cy.fixture("Owl.jpg", { encoding: null }).as("myFixture");
-    cy.fixture("Owl.jpg", { encoding: null }).as("myFixture1");
+    // cy.fixture("Owl.jpg", { encoding: null }).as("myFixture1");
     cy.get(
       ":nth-child(4) > .input-group > .custom-file > .form-control"
       ).selectFile("@myFixture");
-      cy.get('#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(5) > .input-group > .custom-file > .form-control').selectFile(['@myFixture1']);
-        cy.get(".btn-primary").click();
-      });
+    cy.get('#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(5) > .input-group > .custom-file > .form-control').selectFile(['@myFixture']);
+    cy.get(".btn-primary").click();
+  });
       
-      it("Form Inputan Tidak Terisi", () => {
-        
+  it("Form Inputan Tidak Terisi", () => {      
     cy.visit("https://newwebionid.000webhostapp.com/login.php");
     cy.get("#username-field > .form_login").type(email);
     cy.get("#password").type(password);
     cy.get(".btn").click();
-        cy.get(".sidebarCollapse").click();
-        cy.get(".menu-extras > .dropdown-toggle").click();
-        cy.get("#more > :nth-child(1) > a").click();
-        cy.get(".modal-body > :nth-child(1) > .form-control").type("");
-        cy.get('#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(2) > .form-control').type("");
-        cy.get("#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(3) > .form-control").type("");
+    cy.get(".sidebarCollapse").click();
+    cy.get(".menu-extras > .dropdown-toggle").click();
+    cy.get("#more > :nth-child(1) > a").click();
+    cy.get(".modal-body > :nth-child(1) > .form-control").type("");
+    cy.get('#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(2) > .form-control').type("");
+    cy.get("#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(3) > .form-control").type("");
     cy.fixture("", { encoding: null }).as("myFixture2");
-    cy.fixture("", { encoding: null }).as("myFixture3");
+    // cy.fixture("", { encoding: null }).as("myFixture3");
     cy.get(
       ":nth-child(4) > .input-group > .custom-file > .form-control"
     ).selectFile(["myFixture2"]);
     cy.get(
       "#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(5) > .input-group > .custom-file > .form-control"
-    ).selectFile(["myFixture3"]);
+    ).selectFile(["myFixture2"]);
     cy.get(".btn-primary").click();
   });
 
@@ -69,17 +68,16 @@ describe("Ajukan Mentor", () => {
     cy.get(
       "#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(2) > .form-control"
       ).type(last_degree);
-      cy.get(
+    cy.get(
         "#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(3) > .form-control"
         ).type(occupation);
-        cy.fixture("BGUltra.jpg", { encoding: null }).as("myFixture4");
-        cy.fixture("BGUltra.jpg", { encoding: null }).as("myFixture5");
-        cy.get(
+    cy.fixture("BGUltra.jpg", { encoding: null }).as("myFixture4");
+    // cy.fixture("BGUltra.jpg", { encoding: null }).as("myFixture5");
+    cy.get(
           ":nth-child(4) > .input-group > .custom-file > .form-control"
           ).selectFile(["myFixture4"]);
     cy.get(
       "#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(5) > .input-group > .custom-file > .form-control"
-    ).selectFile(['myFixture5']);
+    ).selectFile(['myFixture4']);
   });
-
 });
