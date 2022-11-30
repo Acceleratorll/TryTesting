a45a4c4a7708dc743da4c16ca9cce2c0605bccd9
Moch Fajrul Falah <mfajrulfalah.ff@gmail.com>
2022-11-30 10:40:53 +0700

    fixed

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -18,10 +18,10 @@ describe("Ajukan Mentor", () => {
   const biodata = "Like games";
   const last_degree = "d4";
   const occupation = "none";
-  const bukti_ijazah = "cypress/fixtures/Owl.jpg";
-  const image2 = "cypress/fixtures/Owl.jpg";
-  const bukti_ijazah_small = "cypress/fixtures/Owl.jpg";
-  const image2_small = "cypress/fixtures/Owl.jpg";
+  // const bukti_ijazah = "cypress/fixtures/Owl.jpg";
+  // const image2 = "cypress/fixtures/Owl.jpg";
+  // const bukti_ijazah_small = "cypress/fixtures/Owl.jpg";
+  // const image2_small = "cypress/fixtures/Owl.jpg";
   it("Form Inputan Terisi Sempurna", () => {
     cy.get(".sidebarCollapse").click();
     cy.get(".menu-extras > .dropdown-toggle").click();
@@ -29,28 +29,32 @@ describe("Ajukan Mentor", () => {
     cy.get(".modal-body > :nth-child(1) > .form-control").type(biodata);
     cy.get('#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(2) > .form-control').type(last_degree);
     cy.get("#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(3) > .form-control").type(occupation);
-    cy.get("input[type=file]").selectFile(bukti_ijazah_small, { force: true });
-    cy.get("input[type=file]").selectFile([image2_small, { force: true }]);
-    cy.get(".btn-primary").click();
-  });
-
-  it("Form Inputan Tidak Terisi", () => {
-    cy.get(".sidebarCollapse").click();
-    cy.get(".menu-extras > .dropdown-toggle").click();
-    cy.get("#more > :nth-child(1) > a").click();
-    cy.get(".modal-body > :nth-child(1) > .form-control").type("");
-    cy.get(
-      "#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(2) > .form-control"
-    ).type("");
-    cy.get(
-      "#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(3) > .form-control"
-    ).type("");
+    cy.fixture("Owl.jpg", { encoding: null }).as("myFixture");
+    cy.fixture("Owl.jpg", { encoding: null }).as("myFixture1");
     cy.get(
       ":nth-child(4) > .input-group > .custom-file > .form-control"
-    ).selectFile([""]);
+      ).selectFile("@myFixture");
+      cy.get(
+        "#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(5) > .input-group > .custom-file > .form-control"
+        ).selectFile(['@myFixture1']);
+        cy.get(".btn-primary").click();
+      });
+      
+      it("Form Inputan Tidak Terisi", () => {
+        cy.get(".sidebarCollapse").click();
+        cy.get(".menu-extras > .dropdown-toggle").click();
+        cy.get("#more > :nth-child(1) > a").click();
+        cy.get(".modal-body > :nth-child(1) > .form-control").type("");
+        cy.get('#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(2) > .form-control').type("");
+        cy.get("#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(3) > .form-control").type("");
+    cy.fixture("", { encoding: null }).as("myFixture2");
+    cy.fixture("", { encoding: null }).as("myFixture3");
+    cy.get(
+      ":nth-child(4) > .input-group > .custom-file > .form-control"
+    ).selectFile(["myFixture2"]);
     cy.get(
       "#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(5) > .input-group > .custom-file > .form-control"
-    ).selectFile([""]);
+    ).selectFile(["myFixture3"]);
     cy.get(".btn-primary").click();
   });
 
@@ -61,16 +65,18 @@ describe("Ajukan Mentor", () => {
     cy.get(".modal-body > :nth-child(1) > .form-control").type(biodata);
     cy.get(
       "#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(2) > .form-control"
-    ).type(last_degree);
-    cy.get(
-      "#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(3) > .form-control"
-    ).type(occupation);
-    cy.get(
-      ":nth-child(4) > .input-group > .custom-file > .form-control"
-    ).selectFile([bukti_ijazah]);
+      ).type(last_degree);
+      cy.get(
+        "#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(3) > .form-control"
+        ).type(occupation);
+        cy.fixture("BGUltra.jpg", { encoding: null }).as("myFixture4");
+        cy.fixture("BGUltra.jpg", { encoding: null }).as("myFixture5");
+        cy.get(
+          ":nth-child(4) > .input-group > .custom-file > .form-control"
+          ).selectFile(["myFixture4"]);
     cy.get(
       "#ajukanMentorModal > .modal-dialog > .modal-content > .form-vertical > .modal-body > :nth-child(5) > .input-group > .custom-file > .form-control"
-    ).selectFile([image2]);
+    ).selectFile(['myFixture5']);
   });
 
 });
